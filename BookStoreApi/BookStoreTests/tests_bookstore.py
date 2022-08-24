6e8e25737bd6dd975b20131929b11d7def2e3d75
shay dayan <shay.etl.dayan@gmail.com>
2022-08-24 22:18:32 +0300

    Fixing API to send Object to respond

--- BookStoreApi/BookStoreTests/tests_bookstore.py
+++ BookStoreApi/BookStoreTests/tests_bookstore.py
@@ -13,7 +13,6 @@ import requests
 LOGGER = logging.getLogger(__name__)
 
 
-
 @pytest.fixture(scope="module")
 def bearer_auth_session(user_fix):
     headers = {'accept': 'application/json'}
@@ -53,7 +52,7 @@ def user_fix():
 
 @pytest.fixture(scope="module")
 def get_data_from_json():
-    with open("data.json","r") as f:
+    with open("data.json", "r") as f:
         res_json_data = UserResult(**json.load(f))
     return res_json_data
 
@@ -63,8 +62,8 @@ def post_account(user_fix, accountApi):
     code, outcome = accountApi.post_account(data=user_fix)
     LOGGER.info(outcome)
     if code == 201 and outcome.username == user_fix["userName"]:
-        res_json = json.dumps(outcome.to_json(),indent=3)
-        with open("data.json","w") as f:
+        res_json = json.dumps(outcome.to_json(), indent=3)
+        with open("data.json", "w") as f:
             f.write(res_json)
         return outcome
 
@@ -73,6 +72,7 @@ def test_authorized_user(user_fix, accountApi):
     code, outcome = accountApi.authorized_account(data=user_fix)
     LOGGER.info(outcome)
     assert code == 200 and outcome
+
 
 def test_invalid_post_account(user_fix, accountApi):
     code, outcome = accountApi.post_account(data=user_fix)
@@ -88,17 +88,18 @@ def test_get_by_id(get_data_from_json, accountApi):
 
 @pytest.fixture(scope="module")
 def book_fix():
-    book = {"isbn":"9781449325862"}
+    book = {"isbn": "9781449325862"}
     return CollectionOfIsbn(**book)
+
 
 @pytest.fixture(scope="module")
 def book_fix2():
-    book = {"isbn":"9781449331818"}
+    book = {"isbn": "9781449331818"}
     return CollectionOfIsbn(**book)
 
 
 @pytest.fixture(scope="module")
-def book_list_fix(book_fix, book_fix2,get_data_from_json):
+def book_list_fix(book_fix, book_fix2, get_data_from_json):
     isbn_collection = [book_fix, book_fix2]
     books_list = AddListOfBooks(userId=get_data_from_json.userId, collectionOfIsbns=isbn_collection)
     return books_list
@@ -108,7 +109,7 @@ def book_list_fix(book_fix, book_fix2,get_data_from_json):
 def store_user_fix():
     store_user_json = {
         "userId": "e7fa32a6-ce2c-43bd-a106-7c5031e3141e",
-        "isbn":"9781449331818"
+        "isbn": "9781449331818"
     }
     rep_sbn = ReplaceIsbn(**store_user_json)
     return rep_sbn
@@ -125,7 +126,7 @@ def test_post_books(book_list_fix, bookStoreApi):
     assert code == 201 and book_list_fix == outcome
 
 
-def test_delete_books(get_data_from_json,bookStoreApi):
+def test_delete_books(get_data_from_json, bookStoreApi):
     code, outcome = bookStoreApi.delete_books(userId=get_data_from_json.userId)
     LOGGER.info(outcome)
     assert code == 204
@@ -136,20 +137,20 @@ def test_get_book(book_fix, bookStoreApi):
     assert code == 200 and outcome.isbn == book_fix.isbn
 
 
-def test_delete_book(book_fix, bookStoreApi,get_data_from_json):
+def test_delete_book(book_fix, bookStoreApi, get_data_from_json):
     str_obj = StringObject(isbn=book_fix.isbn, user_id=get_data_from_json.userId)
     code, outcome = bookStoreApi.delete_book(data=str_obj.to_json())
     LOGGER.info(outcome)
     assert code == 204
 
 
-def test_put_books(book_fix2,store_user_fix, bookStoreApi):
+def test_put_books(book_fix2, store_user_fix, bookStoreApi):
     code, outcome = bookStoreApi.put_books(isbn=book_fix2.isbn, data=store_user_fix.to_json())
     LOGGER.info(outcome)
     assert code == 200
 
 
-def test_delete_user(get_data_from_json,accountApi):
+def test_delete_user(get_data_from_json, accountApi):
     code1, outcome1 = accountApi.delete_user(userId=get_data_from_json.userId)
     LOGGER.info(outcome1)
     assert code1 == 204
